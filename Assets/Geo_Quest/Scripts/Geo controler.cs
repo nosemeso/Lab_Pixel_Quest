0f84599ca213ebb0c8dcf2e5678fc72034e1ab30
nosemeso <nosemeso8@gmail.com>
2025-09-27 12:26:16 -0400

    9/27/25

---
+++ Assets/Geo_Quest/Scripts/Geo controler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Geocontroler : MonoBehaviour
+{
+
+    public string VariableOne = "Hello";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Word");
+      
+            string VariableTwo = "World";
+
+       Debug.Log(VariableOne + VariableTwo);
+
+
+    }
+ 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
