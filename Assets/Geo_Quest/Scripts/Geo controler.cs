b550f46e3642b1669bbd2c63a94c91d7178a49f7
nosemeso <nosemeso8@gmail.com>
2025-10-04 15:44:34 -0400

    10/4/25

--- Assets/Geo_Quest/Scripts/Geo controler.cs
+++ Assets/Geo_Quest/Scripts/Geo controler.cs
@@ -1,29 +1,119 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
+
+using System;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Geocontroler : MonoBehaviour
 {
 
     public string VariableOne = "Hello";
-    // Start is called before the first frame update
-    void Start()
+
+    int var = 3;
+
+    private Rigidbody2D kuri;
+
+    public int speed = 5;
+
+    public object SceneMAnager { get; private set; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Hello Word");
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    Debug.Log("Player Has Died");
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+
+           
+
+        }
+
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        //Debug.Log("Hello Word");
       
             string VariableTwo = "World";
 
        Debug.Log(VariableOne + VariableTwo);
 
+        kuri = GetComponent<Rigidbody2D>();
 
     }
- 
 
 
-    // Update is called once per frame
-    void Update()
-    {
+
+        // Update is called once per frame
+        void Update()
+    { 
+        //Debug.Log(var);
+        var++;
+
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            kuri.velocity += new Vector2(-1, kuri.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            kuri.velocity += new Vector2(1, kuri.velocity.y);
+        }
+        */
         
+            float xInput = Input.GetAxis("Horizontal");
+            kuri.velocity = new Vector2(xInput * speed, kuri.velocity.y);
+
+
+
+
+            /*
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        transform.position += new Vector3(0, 1, 0);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        transform.position += new Vector3(0, -1, 0);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        transform.position += new Vector3(-1, 0, 0);
+                    }
+
+                    if (Input.GetKeyDodwn(KeyCode.D))
+                    {
+                        transform.position += new Vector3(1, 0, 0);
+                    }
+
+                */
+
+
+
+
+
+
+
+
+
+        }
+
+    private void GetAxis(string v)
+    {
+        throw new NotImplementedException();
     }
 }
+
+
+
+
+
